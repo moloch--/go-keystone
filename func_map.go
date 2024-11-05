79fefa5b1db805ef3be1aee9e3f8b166cc8f35f6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-05 19:25:45 +0800

    add a import and export function name map for keystone wasm module.

---
+++ func_map.go
@@ -0,0 +1,26 @@
+package keystone
+
+var importModuleName = "a"
+
+var (
+	___cxa_throw            = "a"
+	___syscall_fstat64      = "g"
+	___syscall_getcwd       = "o"
+	___syscall_lstat64      = "d"
+	___syscall_newfstatat   = "e"
+	___syscall_openat       = "p"
+	___syscall_stat64       = "f"
+	__abort_js              = "s"
+	__mmap_js               = "k"
+	__munmap_js             = "l"
+	_emscripten_resize_heap = "t"
+	_environ_get            = "m"
+	_environ_sizes_get      = "n"
+	_exit                   = "h"
+	_fd_close               = "c"
+	_fd_fdstat_get          = "b"
+	_fd_pread               = "i"
+	_fd_read                = "r"
+	_fd_seek                = "j"
+	_fd_write               = "q"
+)
