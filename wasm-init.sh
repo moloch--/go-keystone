e10c6c323ad80b38de738b6d17477c9c1550ddf4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-06 16:20:23 +0800

    rename scripts about init and build keystone wasm module.

---
+++ wasm-init.sh
@@ -0,0 +1,21 @@
+set -eu
+
+sudo yum install -y htop git gcc cmake
+
+git clone https://github.com/emscripten-core/emsdk
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
+cd ..
+
+git clone https://github.com/keystone-engine/keystone
+
+# when use emscripten, it will cost a lot of memory.
+#
+# sudo fallocate -l 8G /swapfile
+# sudo chmod 600 /swapfile
+# sudo mkswap /swapfile
+# sudo swapon /swapfile
+#
+# sudo swapoff /swapfile
+# sudo rm /swapfile
