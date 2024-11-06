dc4d57c0f73c1eff58039da52af77b0979e99ca9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-11-06 17:35:19 +0800

    add tests about Engine_Option, Assemble and Version.

--- keystone_test.go
+++ keystone_test.go
@@ -1,14 +1,84 @@
 package keystone
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewEngine(t *testing.T) {
-	engine, err := NewEngine(ARCH_X86, MODE_64)
+func TestEngine(t *testing.T) {
+	engine, err := NewEngine(ARCH_X86, MODE_32)
 	require.NoError(t, err)
+	err = engine.Option(OPT_SYNTAX, OPT_SYNTAX_INTEL)
+	require.NoError(t, err)
+
+	src := strings.Repeat("xor eax, eax\nret\n", 5000)
+	inst, err := engine.Assemble(src, 0)
+	require.NoError(t, err)
+	expected := bytes.Repeat([]byte{0x31, 0xC0, 0xC3}, 5000)
+	require.Equal(t, expected, inst)
+
+	err = engine.Close()
+	require.NoError(t, err)
+}
+
+func TestEngine_Option(t *testing.T) {
+	engine, err := NewEngine(ARCH_X86, MODE_32)
+	require.NoError(t, err)
+
+	t.Run("common", func(t *testing.T) {
+		err = engine.Option(OPT_SYNTAX, OPT_SYNTAX_INTEL)
+		require.NoError(t, err)
+	})
+
+	t.Run("invalid option type", func(t *testing.T) {
+		err = engine.Option(123, OPT_SYNTAX_INTEL)
+		errStr := "failed to set keystone option: Invalid option (KS_ERR_OPT_INVALID)"
+		require.EqualError(t, err, errStr)
+	})
+
+	t.Run("invalid option value", func(t *testing.T) {
+		err = engine.Option(OPT_SYNTAX, 123)
+		errStr := "failed to set keystone option: Invalid option (KS_ERR_OPT_INVALID)"
+		require.EqualError(t, err, errStr)
+	})
+
+	err = engine.Close()
+	require.NoError(t, err)
+}
+
+func TestEngine_Assemble(t *testing.T) {
+	engine, err := NewEngine(ARCH_X86, MODE_32)
+	require.NoError(t, err)
+
+	t.Run("common", func(t *testing.T) {
+		src := "xor eax, eax\nret\n"
+		inst, err := engine.Assemble(src, 0)
+		require.NoError(t, err)
+		expected := []byte{0x31, 0xC0, 0xC3}
+		require.Equal(t, expected, inst)
+	})
+
+	t.Run("invalid source", func(t *testing.T) {
+		src := "invalid\n"
+		inst, err := engine.Assemble(src, 0)
+		errStr := "failed to assemble: Invalid mnemonic (KS_ERR_ASM_MNEMONICFAIL)"
+		require.EqualError(t, err, errStr)
+		require.Nil(t, inst)
+	})
+
+	err = engine.Close()
+	require.NoError(t, err)
+}
+
+func TestEngine_Version(t *testing.T) {
+	engine, err := NewEngine(ARCH_X86, MODE_32)
+	require.NoError(t, err)
+
+	version := engine.Version()
+	require.Equal(t, "0.9", version)
 
 	err = engine.Close()
 	require.NoError(t, err)
